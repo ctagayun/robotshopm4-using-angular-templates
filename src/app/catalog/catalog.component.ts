c4880a71d6fa7023badfe1452f8114606f6f4133
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-10-28 19:05:15 -0500

    save

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProduct } from './product.model';
 
 @Component({
   selector: 'bot-catalog',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./catalog.component.css']
 })
 export class CatalogComponent {
+  product: IProduct;
+  filter: string = '';
 
+  constructor(){
+    //Initialize product property. Will use hardcoded code for now
+    //for now we will display this in the CatalogComponent.ts using
+    //interpolation
+    this.product = {
+        id: 1,
+        description:
+          "A robot head with an unusually large eye and teloscpic neck -- excellent for exploring high spaces.",
+        name: "Large Cyclops",
+        imageName: "head-big-eye.png",
+        category: "Heads",
+        price: 1220.5,
+        discount: 0.2,
+      };
+  }
 }
---
+++ src/app/catalog/product.model.ts
@@ -0,0 +1,10 @@
+//this can be used as data type for my product property in CatalogComponent
+export interface IProduct {
+  id: number;
+  description: string;
+  name: string;
+  imageName: string;
+  category: string;
+  price: number;
+  discount: number;
+}
